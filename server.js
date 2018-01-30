b9f97f7651f38128974af4aa2c1c979d8f2dedcb
gdelegue <gerard.delegue@nokia.com>
2018-01-30 15:12:36 +0000

    .

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http');
 
+var server_port =  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
   response.end('Hello World!');
 };
 var www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(server_port, server_ip_address);
