f785db00063c8f59be13695fd4cc442f133578ae
gdelegue <gerard.delegue@alcatel-lucent.com>
2018-02-02 10:19:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/temperature/:value', function(req, res) {
 
    // t_fahrenheit = .... TO BE COMPLETED
   
-   t_fahrenheit  = ;
+   t_fahrenheit  = 0;
 
    //
    //res.send('{"fahrenheit": '+ t_fahrenheit +'}');
